ed3aa0513fb15f4ebadd453c3c30d652db10e47c
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2022-03-25 19:25:30 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Unicorn = require('./Unicorn')
-const DB_URL = "mongodb+srv://admin:admin@cluster0.mdsan.mongodb.net/unciorns_App?retryWrites=true&w=majority"
+const DB_URL = "mongodb+srv://root:root@cluster0.07urh.mongodb.net/unicorns-app?retryWrites=true&w=majority"
 
 mongoose.connect(DB_URL, (err)=> {
   if(err){
