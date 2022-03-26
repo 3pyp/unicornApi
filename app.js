09c2a272874af99aebc7be13f6f6d600b33675fb
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2022-03-25 19:20:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const router = require('./routes');
+const cors = require('cors')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 app.use('/api/v1', router);
 
 app.listen(PORT, () => {
