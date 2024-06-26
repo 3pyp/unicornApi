6a258340fb47b5c981b24b80a7fd69ee99f4769d
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2024-06-26 14:44:01 -0500

    Se agrega la validacion para la edad

--- routes/index.js
+++ routes/index.js
@@ -38,12 +38,26 @@ router.put("/unicorns/:unicornid", (req, res) => {
   const { name, power, age, image } = req.body;
   let data = {};
 
-  if (name !== undefined && name !== null && name !== "") data["name"] = name;
-  if (power !== undefined && power !== null && power !== "")
+  if (age !== undefined && age !== null && age !== "") {
+    try {
+      const parsedValue = parseInt(age);
+      if (isNaN(parsedValue)) {
+        res.status(304).send("Invalid number");
+      }
+      data["age"] = parsedValue;
+    } catch (error) {
+      res.status(304).send(error);
+    }
+  }
+  if (name !== undefined && name !== null && name !== "") {
+    data["name"] = name;
+  }
+  if (power !== undefined && power !== null && power !== "") {
     data["power"] = power;
-  if (age !== undefined && age !== null && age !== "") data["age"] = age;
-  if (image !== undefined && image !== null && image !== "")
+  }
+  if (image !== undefined && image !== null && image !== "") {
     data["image"] = image;
+  }
 
   Unicorn.findById(req.params.unicornid)
     .then((dbunicorn) => {
